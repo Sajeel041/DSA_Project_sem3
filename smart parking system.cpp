595ca28da386727d2fbecffaedd8deb41d9dc5e6
Sajeel041 <sajeelm041@gmail.com>
2024-12-21 21:40:38 +0500

    adding parkingspot and Vehical
    these are the two structs that will be used as objects in every other classes and functions

--- smart parking system.cpp
+++ smart parking system.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+// Struct to represent a Parking Spot
+struct ParkingSpot {
+    int id;
+    bool isAvailable;
+    SlotSize size; 
+    double distanceFromEntrance; 
+    double baseRate;             
+    double ratePerHour;          
+};
+
+// Struct to represent a Vehicle
+struct Vehicle {
+    
+    int driverID;
+    string licenseNumber;
+    VehicleType type;
+    double entryTime;    
+};
+int main(){
+  
+return 0;
+}
