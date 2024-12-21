dc152131a6da0a907abdc83f8926012563b28d2b
Sajeel041 <sajeelm041@gmail.com>
2024-12-21 22:27:47 +0500

    switch cases
    just added ssome basic  switch casees through which i can access the claases and implement the changes

--- smart parking system.cpp
+++ smart parking system.cpp
@@ -19,7 +19,117 @@ struct Vehicle {
     VehicleType type;
     double entryTime;    
 };
+class SmartParkingManagement{
+
+};
+class Driver : public SmartParkingManagement{
+
+};
+class ParkingLotManager : public SmartParkingManagement {
+
+};
+class Admin{
+
+};
+
 int main(){
-  
+    int choice;
+  do {
+        cout << "\n=== Main Menu ===\n";
+        cout << "Select Role:\n";
+        cout << "1. Driver\n";
+        cout << "2. Parking Lot Manager\n";
+        cout << "3. Admin\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+
+      cin>>choice;
+      switch(choice){
+              case 1: { // Driver
+                int driverChoice;
+                do {
+                    cout << "\n=== Driver Menu ===\n";
+                    cout << "1. Display Available Spots\n";
+                    cout << "2. Reserve Spot\n";
+                    cout << "3. Release Spot\n";
+                    cout << "4. Back to Main Menu\n";
+                    cout << "Enter your choice: ";
+                    switch(driverChoice){
+                        case 1:{}
+                        case 2:{
+                        }
+                        case 3:{}
+                        case 4:{}
+                        default:
+                    }
+                }(while (driverChoice != 4);
+                  break;
+                }
+                    case 2:{ // Parking Lot Manager
+                        string managerName;
+                        cout << "Enter Manager Name: ";
+                        cin >> managerName;
+                        int managerChoice;
+                    do {
+                        cout << "\n=== Parking Lot Manager Menu ===\n";
+                        cout << "1. Display Available Spots\n";
+                        cout << "2. Add Parking Spot\n";
+                        cout << "3. Update Parking Spot\n";
+                        cout << "4. Back to Main Menu\n";
+                        cout << "Enter your choice: ";
+                        switch(managerChoice){
+                            case 1:{}
+                        case 2:{
+                        }
+                        case 3:{}
+                        case 4:{}
+                        default:
+                            
+                        }
+                    }while(mangerchoice!=4);
+                        break;
+                    }
+                    case 3:{ // Parking Lot Manager
+                        string managerName;
+                        cout << "Enter Manager Name: ";
+                        cin >> managerName;
+                        int managerChoice;
+                    do {
+                        cout << "\n=== Admin Menu ===\n";
+                        cout << "1. Add Manager\n";
+                        cout << "2. Remove Manager\n";
+                        cout << "3. Change Security Code\n";
+                        cout << "4. Back to Main Menu\n";
+                        cout << "Enter your choice: ";
+                        switch(managerChoice){
+                            case 1:{}
+                        case 2:{
+                        }
+                        case 3:{}
+                        case 4:{}
+                        default:
+                            
+                        }
+                    }while(mangerchoice!=4);
+                        break;
+                    }
+                    case 4: { // Exit
+                    driver.saveData(); // Save data before exiting
+                    cout << "Exiting the system. Goodbye!\n";
+                    break;
+                    }
+                    default:
+                    cout << "Invalid choice. Please try again.\n";
+                    }
+
+                    }
+
+          }
+
+          
+      }
+
+  }while(choice !=4);
+    
 return 0;
 }
